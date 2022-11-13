0b021d65ff60c589607d069b1692dfa08c6872dd
Gabriella Huzyk <gabriellahuzyk@gmail.com>
2022-11-12 22:56:26 -0300

    add dto's

---
+++ src/users/dto/create.user.dto.ts
@@ -0,0 +1,24 @@
+import { IsBoolean, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  readonly first_name: string;
+
+  @IsString()
+  readonly last_name: string;
+
+  @IsString()
+  readonly store_name: string;
+
+  @IsString()
+  readonly email: string;
+
+  @IsString()
+  readonly password: string;
+
+  @IsString()
+  readonly phone_number: string;
+
+  @IsBoolean()
+  readonly store_visibel: boolean;
+}
